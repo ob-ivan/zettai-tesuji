6d61b39b2a1aa549cf7511c823952b9f45a970a9
ob-ivan <ob-ivan@github.com>
2013-02-07 22:53:28 +0400

    Сделал hello world на фреймворке Silex

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-header('Location: https://github.com/ob-ivan/zettai-tesuji');
+$time = microtime(true);
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new Silex\Application();
+
+// $app['debug'] = true; // раскомментируй на деве, а ещё лучше -- внеси в конфиг хоста.
+
+$app->get('/', function () {
+    return 'hello world';
+});
+
+$app->run();
+
+print '<!-- server time: ' . (microtime(true) - $time) . ' -->';
