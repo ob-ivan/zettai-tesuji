0da0b46b98f7da9eca6ce3edb75278d38860c562
ob-ivan <ob-ivan@github.com>
2013-02-08 00:21:34 +0400

    Ввожу концепцию конфигов хостов. Дополнительная защита от веб-доступа

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 $time = microtime(true);
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Загружаем конфиги.
+
+// Инициализируем приложение.
+
 $app = new Silex\Application();
 
 // $app['debug'] = true; // раскомментируй на деве, а ещё лучше -- внеси в конфиг хоста.
