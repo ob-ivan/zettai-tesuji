86f2619bf34dff1414e413a043f176c467ac9db6
ob-ivan <ob-ivan@github.com>
2013-02-13 01:42:33 +0400

    Пытаюсь улучшить обработчик хука

--- githook.php
+++ githook.php
@@ -2,6 +2,18 @@
 define('LOG_DIR', 'log');
 define('LOG_PATH', LOG_DIR . '/githook.log');
 
+function writeLog ($message)
+{
+    if (! is_dir (LOG_DIR)) {
+        mkdir (LOG_DIR, 0777, true);
+    }
+    file_put_contents(LOG_PATH, '[' . date('Y-m-d H:i:s') . '] ' . $message . "\n", FILE_APPEND);
+}
+
+set_error_handler(function ($errno, $errstr, $errfile, $errline ) {
+    throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+});
+
 try {
     $payload = json_decode($_POST['payload']);
 } catch (Exception $e) {
@@ -9,14 +21,18 @@ try {
 }
 
 // log the request
-if (! is_dir (LOG_DIR)) {
-    mkdir (LOG_DIR, 0777, true);
-}
-file_put_contents(LOG_PATH, '[' . date('Y-m-d H:i:s') . '] payload = ' . print_r($payload, true), FILE_APPEND);
+writeLog('payload = ' . print_r($payload, true));
 
 // only execute if pushed to master
 if ($payload->ref === 'refs/heads/master')
 {
+    writeLog('ref is ok, trying to exec(deploy.sh)');
+    
     // run deployment script
-    exec('./deploy.sh');
+    try {
+        $output = shell_exec('./deploy.sh');
+        writeLog('returned from exec(deploy.sh), output = ' . $output);
+    } catch (Exception $e) {
+        writeLog('caught exception = ' . print_r($e, true));
+    }
 }
