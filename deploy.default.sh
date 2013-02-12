820ed84b6c038aad3e063ce8aca319d902a37db3
ob-ivan <ob-ivan@github.com>
2013-02-13 00:20:07 +0400

    Пытаюсь улучшить образец скрипта развёртывания

--- deploy.default.sh
+++ deploy.default.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env /bin/bash
+
+### Скопируйте этот файл deploy.sh и настройте значения переменных.
+
+### Укажите команду для запуска composer.
+# COMPOSER=composer
+
 git pull
-composer update
+$COMPOSER update
