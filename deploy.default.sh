fab25da45955b12f8095ce79ededd6498dd257d2
ob-ivan <ob-ivan@github.com>
2013-02-13 00:53:10 +0400

    Поправил опечатку

--- deploy.default.sh
+++ deploy.default.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env /bin/bash
 
-### Скопируйте этот файл deploy.sh и настройте значения переменных.
+### Скопируйте этот файл в deploy.sh и настройте значения переменных.
 
 ### Укажите команду для запуска composer.
 # COMPOSER="/usr/local/php54/bin/php $HOME/bin/composer.phar --working-dir=$PWD"
