5c5b05efaa901be95fd4a11de93b9a0893c56906
ob-ivan <ob-ivan@github.com>
2013-02-20 23:00:23 +0400

    Добавил обработчик csrf-токенов

---
+++ src/Zettai/CsrfHandler.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Обработчик и валидатор csrf-токенов.
+ *
+ * Порождает токены и сохраняет их в сессию.
+ * Отвечает на вопрос, соответствует ли токен ранее сгенерированному значению.
+ *
+ * Как применять:
+ *  - При выводе формы вставляем токен:
+ *      <input type="hidden" name="csrf" value="{{ csrfHandler.generate('operation_name_' ~ operation_id) }}"/>
+ *  - В обработчике формы сравниваем токен с ранее сохранённым:
+ *      if (! $csrfHandler->validate ($request->request('csrf'), 'operation_name_' . $operation_id)) {
+ *          throw new Exception('Попытка взлома');
+ *      }
+**/
+namespace Zettai;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class CsrfHandler
+{
+    // const //
+    
+    const SALT = 'KSt;tE:0c-j9N5_6';
+    
+    // public //
+    
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+    
+    /**
+     * Порождает хэш для вывода в форму.
+    **/
+    public function generate($seed)
+    {
+        $csrf = $this->hash($seed);
+        $this->session->set($csrf, $seed);
+        return $csrf;
+    }
+    
+    /**
+     * Проверяет, соответствует ли переданный формой хэш
+     * ранее сохранённому значению.
+    **/
+    public function validate($csrf, $test)
+    {
+        if (! $this->session->has($csrf)) {
+            return false;
+        }
+        $seed = $this->session->get($csrf);
+        return $seed === $test;
+    }
+    
+    // private //
+    
+    private function hash($seed)
+    {
+        return md5($seed . microtime(true) . self::SALT);
+    }
+}
