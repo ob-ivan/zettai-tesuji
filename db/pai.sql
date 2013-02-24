3f6f08c446db4da3c43ef90fa17422fc969e65bf
ob-ivan <ob-ivan@github.com>
2013-02-24 22:04:13 +0400

    Решительно приступил к переделке хранения структуры данных о задачи на джсон

--- db/pai.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `pai` (
-  `pai_id` tinyint(2) unsigned NOT NULL auto_increment,
-  `display` varchar(2) NOT NULL,
-  PRIMARY KEY  (`pai_id`),
-  UNIQUE KEY `display` (`display`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
