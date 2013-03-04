31553e4c9b1c5e7589194cc7d7fee4a2dc41fb5b
ob-ivan <ob-ivan@github.com>
2013-03-04 23:44:03 +0400

    Добавил чтение указанного в командной строке ресурса

--- bin/index.php
+++ bin/index.php
@@ -1,10 +1,14 @@
 <?php
 
-// Пути.
+// Настраиваем окружение.
 define ('DOCUMENT_ROOT', dirname(__DIR__));
 define ('AUTOLOAD_PATH', DOCUMENT_ROOT . '/vendor/autoload.php');
 
-// Зависимости.
+set_error_handler(function ($errno, $errstr, $errfile, $errline ) {
+    throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+});
+
+// Подключаем зависимости.
 require_once AUTOLOAD_PATH;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
@@ -16,7 +20,22 @@ $app = new Zettai\Application(new Zettai\Config(DOCUMENT_ROOT));
 // Задаём рутинг и контроллеры.
 
 $app->get('/import', function (Request $request) use ($app) {
-    // TODO: Прочитать либо имя файла, либо адрес треда из $request->query->get(0).
+    // Прочитать либо имя файла, либо адрес треда из параметров.
+    $filepath = $request->query->get(0);
+    if (empty ($filepath)) {
+        return
+            'USAGE: run.sh import FILENAME' . "\n" .
+            'OR:    run.sh import URL';
+    }
+    // Достать содержимое.
+    try {
+        $contents = file_get_contents($filepath);
+    } catch (Exception $e) {
+        return 'Could not get contents of "' . $filepath . '": ' .
+            get_class($e) . ' (' . $e->getCode() . ') "' . $e->getMessage() . '"';
+    }
+    unset($filepath);
+    // Разобрать содержимое.
     return 'done';
 });
 
