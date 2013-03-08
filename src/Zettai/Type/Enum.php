c5cac1778937596394fa76f297040d91b91179f6
ob-ivan <ob-ivan@github.com>
2013-03-08 23:18:39 +0400

    Реализовал в перечислимом типе некоторые волшебные методы

--- src/Zettai/Type/Enum.php
+++ src/Zettai/Type/Enum.php
@@ -22,9 +22,36 @@ class Enum extends Type
     }
     
     /**
+     * Волшебные методы:
+     *  - from<View>($presentation)
+     *  - new<Primitive>()
+    **/
+    public function __call($name, $args)
+    {
+        if (preg_match('/^from(\w+)$/i', $name, $matches)) {
+            if (! isset($args[0])) {
+                throw new Exception(
+                    'Method "' . $name . '" expects at least one argument',
+                    Exception::ENUM_FROM_VIEW_ARGUMENT_ABSENT
+                );
+            }
+            $view = $service->getViewByName($matches[1]);
+            if (! $view) {
+                throw new Exception(
+                    'Unknown view "' . $matches[1] . '"',
+                    Exception::ENUM_FROM_VIEW_UNKNOWN_VIEW
+                );
+            }
+            return $this->fromView($view, $args[0]);
+        }
+        // TODO: new<Primitive>
+    }
+    
+    /**
      * Возвращает список значений как объектов.
      *
      * TODO: Добавить обработку коллбэком.
+     * TODO: Кэшировать отдачу.
      *
      *  @return [Value]
     **/
@@ -35,6 +62,42 @@ class Enum extends Type
             $return[$primitive] = $this->fromPrimitive($primitive);
         }
         return $return;
+    }
+    
+    /**
+     * Пытается распознать в аргументе какое-либо из значений.
+     *
+     * TODO: Кэшировать отображение.
+     *
+     *  @param  mixed   $input
+     *  @return Value
+    **/
+    public function from($input)
+    {
+        foreach ($this->service->getViews() as $view) {
+            $candidate = $this->fromView($view, $input);
+            if ($candidate) {
+                return $candidate;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * TODO: Кэшировать отображение.
+    **/
+    public function fromView($view, $input)
+    {
+        $viewIndex = array_search($view, $this->views);
+        if (! $viewIndex) {
+            return null;
+        }
+        foreach ($this->values as $primitive => $views) {
+            if ($views[$viewIndex] === $args[0]) {
+                return $this->fromPrimitive($primitive);
+            }
+        }
+        return null;
     }
     
     public function fromPrimitive($primitive)
--- src/Zettai/Type/Exception.php
+++ src/Zettai/Type/Exception.php
@@ -8,4 +8,7 @@ class Exception extends ParentException
     const SERVICE_SET_OFFSET_ALREADY_EXISTS = 0x0201;
     const SERVICE_SET_VALUE_WRONG_TYPE      = 0x0202;
     const SERVICE_UNSET_UNSUPPORTED         = 0x0203;
+    
+    const ENUM_FROM_VIEW_ARGUMENT_ABSENT    = 0x0204;
+    const ENUM_FROM_VIEW_UNKNOWN_VIEW       = 0x0205;
 }
