3b5dfa8da4bb6b8ae5ae150cf38e9e1f4f689d84
ob-ivan <ob-ivan@github.com>
2013-03-26 00:49:02 +0400

    Добавил в шаблоны блок для добавления скриптов. Добавил абстрактный построитель классов. Сделал варианты ответов кликабельными

--- script/site/exercise.js
+++ script/site/exercise.js
@@ -1,26 +1,32 @@
 
+// require: jquery.js, class.js
 /**
  * Поведение страницы /exercise/{exercise_id}.
  *
- * Когда пользователь выбирает один из ответов,
- * у контроллера /exercise/answer/{exercise_id}
- * 
+ * Когда пользователь выбирает один из ответов:
+ *  - У контроллера /exercise/answer/{exercise_id} запрашиваются авторские ответы.
+ *  - Показываются все ответы, выбранный пользователем ответ подсвечен.
+ *  - Показывается навигация к следующей задаче.
 **/
-var ExercisePage = (function() {
-    
-    var methods = {
-        // <method> : function <method> (<arguments>) { <body> },
-    };
-    
-    // constructor
-    return function ExercisePage(exercise_id) {
-        var private = {
-            exercise_id : exercise_id
-        };
+var ExercisePage = Class({
+    /**
+     * Конструирует объект поведения, навешивает обработчики.
+     *
+     *  @param  {
+     *      exercise_id : <integer>,
+     *      buttons     : <jQuery>
+     *  } options
+    **/
+    __construct : function (options) {
+        this.exercise_id = parseInt(options.exercise_id, 10);
         
-        for (var method in methods) {
-            this[method] = private[method] = methods[method].bind(private);
-        }
-    };
-})();
+        var handle = this._handle;
+        options.buttons.on('click', function (event) {
+            handle(this, event);
+        });
+    },
+    _handle : function (element, event) {
+        alert(1); // debug
+    }
+});
 
