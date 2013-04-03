ff46025a52f31a9d4f3b9356b84310145fe69838
ob-ivan <ob-ivan@github.com>
2013-04-04 00:04:06 +0400

    Реализовал ViewFactory

--- src/Ob_Ivan/EviType/ViewFactory.php
+++ src/Ob_Ivan/EviType/ViewFactory.php
@@ -1,20 +1,62 @@
 <?php
 namespace Ob_Ivan\EviType;
 
-use ArrayAccess;
-
-class ViewFactory implements ArrayAccess
+class ViewFactory
 {
     /**
-     * @var [<string name> => <View(args...) producer>]
+     * @var [<string name> => <TypeSortInterface() producer>]
     **/
-    private $producers = [];
-    
-    public function offsetExists($offset) {}
-    public function offsetGet($offset) {}
-    public function offsetSet($offset, $value) {}
-    public function offsetUnset($offset) {}
-    
-    public function __call($name, $args) {}
-    public function __invoke($name, $args) {}
+    private $registry = [];
+
+    /**
+     * @var [<string name> => <TypeSortInterface sort>]
+    **/
+    private $sorts = [];
+
+    public function produce($name, $arguments)
+    {
+        if (! isset($this->sorts[$name])) {
+            if (! isset($this->registry[$name])) {
+                throw new Exception(
+                    'Unknown view sort "' . $name . '"',
+                    Exception::VIEW_FACTORY_PRODUCE_NAME_UNKNOWN
+                );
+            }
+            $sort = $this->registry[$name]();
+            if (! $sort instanceof ViewSortInterface) {
+                throw new Exception(
+                    'Value for "' . $name . '" must implement ViewSortInterface',
+                    Exception::VIEW_FACTORY_PRODUCE_SORT_WRONG_TYPE
+                );
+            }
+            $this->sorts[$name] = $sort;
+        }
+        return $this->sorts[$name]->produce($arguments);
+    }
+
+    /**
+     * Регистрирует один или несколько сортов представлений.
+     *
+     *  @param  string              $name
+     *  @param  ViewSortInterface() $producer
+     * OR
+     *  @param  [string => ViewSortInterface()] $producerMap
+    **/
+    public function register($name, callable $producer = null)
+    {
+        if (is_array($name)) {
+            foreach ($name as $sortName => $producer) {
+                $this->register($sortName, $producer);
+            }
+        } else {
+            if (isset($this->sorts[$name]) || isset($this->registry[$name])) {
+                throw new Exception(
+                    'View sort "' . $name . '" already exists',
+                    Exception::VIEW_FACTORY_REGISTRY_NAME_ALREADY_EXISTS
+                );
+            }
+            $this->registry[$name] = $producer;
+        }
+        return $this;
+    }
 }
