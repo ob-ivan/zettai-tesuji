7fa94d0586cbfa0a7c2f00dbd2ad9bdf9a05bf33
ob-ivan <ob-ivan@github.com>
2013-04-10 23:18:27 +0400

    Добавил метод Theme::get

--- src/Zettai/Entity/Theme.php
+++ src/Zettai/Entity/Theme.php
@@ -37,4 +37,39 @@ class Theme extends Entity
         // execute
         $this->queryBuilder()->delete(['theme_id' => $theme_id]);
     }
+
+    /**
+     *  @param  integer $theme_id
+     *  @return Value
+    **/
+    public function get($theme_id)
+    {
+        // prepare
+        $theme_id  = intval($theme_id);
+
+        // validate
+        if (! ($theme_id > 0)) {
+            throw new Exception('Theme id is empty', Exception::THEME_ID_EMPTY);
+        }
+
+        // execute
+        $row = $this->queryBuilder()
+        ->select('theme_id')
+        ->select('title')
+        ->select('intro')
+        ->select('min_exercise_id')
+        ->select('max_exercise_id')
+        ->select('advanced_percentage')
+        ->select('intermediate_percentage')
+        ->where(function($expression) {
+            return $expression->equals('theme_id', ':theme_id');
+        })
+        ->fetchAssoc(['theme_id' => $theme_id]);
+
+        // convert to record, if possible.
+        if (! $row) {
+            return null;
+        }
+        return $this->type->from($row);
+    }
 }
