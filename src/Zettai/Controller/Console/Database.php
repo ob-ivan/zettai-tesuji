b3d91f737593b4bcd8acb54c1e4a91a829c32f51
ob-ivan <ob-ivan@github.com>
2013-04-16 10:18:32 +0400

    Реализовал контроллер обновления поля is_answered в задачах

--- src/Zettai/Controller/Console/Database.php
+++ src/Zettai/Controller/Console/Database.php
@@ -9,13 +9,19 @@ use Silex\ControllerProviderInterface;
 
 class Import implements ControllerProviderInterface
 {
+    // var //
+
+    private $model;
+
     // public : ControllerProviderInterface //
 
     public function connect(Application $app)
     {
+        $this->model = $app['model'];
+
         $controllers = $app['controllers_factory'];
-        $controllers->get('/is_answered', function () {
-            return $this->is_answered();
+        $controllers->get('/set_is_answered', function () {
+            return $this->set_is_answered();
         });
         return $controllers;
     }
@@ -25,7 +31,21 @@ class Import implements ControllerProviderInterface
     /**
      * Проставляет всем задачам в базе флаг content.is_unasnwered значнием "истина".
     **/
-    private function is_answered()
+    private function set_is_answered()
     {
+        $entity = $this->model->exercise;
+        $count  = $entity->getCount(true);
+        $perPage = 20;
+        $totalPages = ceil($count / $perPage);
+        for ($pageNum = 0; $pageNum < $totalPages; ++$pageNum) {
+            print 'Page #' . $pageNum;
+            foreach ($entity->getList($pageNum * $perPage, $perPage, true) as $exercise) {
+                print '.';
+                $exercise->content['is_answered'] = true;
+                $entity->set($exercise);
+            }
+            print ' done!' . "\n";
+        }
+        return 'Done.';
     }
 }
